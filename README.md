a4cb04358b7757b68e59073a8f077c97463d0cc3
NTBBloodbath <bloodbathalchemist@protonmail.com>
2025-07-06 20:26:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Norgdo
+
+Get your tasks done using Norg, straight from your terminal.
---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  description = "Get your tasks done usign Norg, straight from your terminal";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable&shallow=1";
+    flake-utils.url = "github:numtide/flake-utils?shallow=1";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+        pkgs = import nixpkgs {inherit system;};
+        toolchain = pkgs.rustPlatform;
+        cargoPackage = (pkgs.lib.importTOML "${self}/Cargo.toml").package;
+      in rec
+      {
+        # nix build
+        packages.default = toolchain.buildRustPackage {
+          pname = cargoPackage.name;
+          version = cargoPackage.version;
+          src = pkgs.lib.cleanSource "${self}";
+          cargoLock = {
+            lockFile = "${self}/Cargo.lock";
+            allowBuiltinFetchGit = true;
+          };
+          useNextest = true;
+          dontUseCargoParallelTests = true;
+
+          meta = {
+            description = cargoPackage.description;
+            homepage = cargoPackage.repository;
+            license = pkgs.lib.licenses.gpl2Only;
+            maintainers = cargoPackage.authors;
+          };
+
+          # For other makeRustPlatform features see:
+          # https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/rust.section.md#cargo-features-cargo-features
+        };
+
+        # nix run
+        apps.default = flake-utils.lib.mkApp {drv = packages.default;};
+
+        # nix develop
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (with toolchain; [
+              cargo
+              rustc
+              rustLibSrc
+            ])
+            clippy
+            rustfmt
+            cargo-edit
+            cargo-nextest
+            rust-analyzer
+          ];
+
+          # Many editors rely on this rust-src PATH variable
+          RUST_SRC_PATH = "${toolchain.rustLibSrc}";
+        };
+      });
+}
